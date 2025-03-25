5cb9cdc987fdb7952916dd1343ad5a0964fe8451
김의현 <hyunkim6790@gmail.com>
2025-03-25 18:14:18 +0900

    feat(Calculator): 연산자로 숫자 구분 기능 추가

--- src/main/kotlin/calculator/Calculator.kt
+++ src/main/kotlin/calculator/Calculator.kt
@@ -1,13 +1,10 @@
 package calculator
 
-/*
-    1주차에만 제공되는 예시 코드입니다.
-    코드는 그대로 사용해도 되고 수정해도 됩니다.
- */
-
 class Calculator {
     fun calculate(input: String): Int {
         // TODO: 코드 구현
+        val num = input.split('+', '-', '*', '/')
+        println(num.joinToString(" "))
         throw IllegalArgumentException("아직 구현되지 않았습니다.")
     }
 }
