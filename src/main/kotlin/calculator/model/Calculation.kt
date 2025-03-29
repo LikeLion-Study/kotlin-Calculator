f0b42f3e21f54b7053513b384fd6e70414d2f262
uihyeon <hyunkim6790@gmail.com>
2025-03-29 22:12:12 +0900

    refactor(Calculation): 하드코딩 상수화

--- src/main/kotlin/calculator/model/Calculation.kt
+++ src/main/kotlin/calculator/model/Calculation.kt
@@ -2,21 +2,30 @@ package calculator.model
 
 class Calculation {
     fun sequence(numbers: List<Int>, operators: List<String>): Int {
-        if (numbers.isEmpty()) return 0
+        if (numbers.isEmpty()) return INITIAL_EMPTY_VALUE
 
         var result = numbers.first()
-        var index = 1
+        var index = INITIAL_INDEX
 
         operators.forEach { operator ->
             when(operator) {
-                "+" -> result += numbers[index++]
-                "-" -> result -= numbers[index++]
-                "*" -> result *= numbers[index++]
-                "/" -> result /= numbers[index++]
+                ADDITION -> result += numbers[index++]
+                SUBTRACTION -> result -= numbers[index++]
+                MULTIPLICATION -> result *= numbers[index++]
+                DIVISION -> result /= numbers[index++]
             }
         }
         return result
     }
 
     fun priority(numbers: List<Int>, operators: List<String>) {}
+
+    companion object {
+        const val INITIAL_INDEX = 1
+        const val INITIAL_EMPTY_VALUE = 0
+        const val ADDITION = "+"
+        const val SUBTRACTION = "-"
+        const val MULTIPLICATION = "*"
+        const val DIVISION = "/"
+    }
 }
