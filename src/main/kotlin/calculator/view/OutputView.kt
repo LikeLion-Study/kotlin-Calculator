06a42d4f0020e064a30521874119734d45b1bb4a
uihyeon <hyunkim6790@gmail.com>
2025-03-29 16:54:11 +0900

    feat(OutputView): 수식 입력 안내 메시지 구현

--- src/main/kotlin/calculator/view/OutputView.kt
+++ src/main/kotlin/calculator/view/OutputView.kt
@@ -1,5 +1,11 @@
 package calculator.view
 
 class OutputView {
-    fun printCalculateMessage() = println("수식을 입력해 주세요.")
+    fun printCalculateMessage() {
+        println(CALCULATE_EXECUTE_MESSAGE)
+    }
+
+    companion object {
+        const val CALCULATE_EXECUTE_MESSAGE = "수식을 입력해 주세요."
+    }
 }
