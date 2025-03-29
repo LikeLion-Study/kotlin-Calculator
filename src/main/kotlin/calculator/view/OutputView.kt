d976f92272fd76f11c9fda7f3d5b61adf63f3f12
uihyeon <hyunkim6790@gmail.com>
2025-03-29 16:57:11 +0900

    feat(OutputView): 입력 에러처리 및 연산 결과 출력 기능 구현

--- src/main/kotlin/calculator/view/OutputView.kt
+++ src/main/kotlin/calculator/view/OutputView.kt
@@ -5,7 +5,16 @@ class OutputView {
         println(CALCULATE_EXECUTE_MESSAGE)
     }
 
+    fun printOperationResults(result: Int) {
+        println(RESULT_FORMAT)
+    }
+
+    fun printErrorMessage(message: String?) {
+        println(message)
+    }
+
     companion object {
         const val CALCULATE_EXECUTE_MESSAGE = "수식을 입력해 주세요."
+        const val RESULT_FORMAT = "결과: %d"
     }
 }
