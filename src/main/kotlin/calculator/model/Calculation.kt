2a19b1c594314e656e89619a1c888d744a0e2c5f
uihyeon <hyunkim6790@gmail.com>
2025-04-01 00:29:38 +0900

    feat(Calculation): 연산자 우선순위 계산 함수 구현 및 리팩토리

--- src/main/kotlin/calculator/model/Calculation.kt
+++ src/main/kotlin/calculator/model/Calculation.kt
@@ -1,28 +1,52 @@
 package calculator.model
 
-class Calculation {
-    fun sequence(numbers: List<Int>, operators: List<String>): Int {
-        if (numbers.isEmpty()) return INITIAL_EMPTY_VALUE
+import calculator.utils.Const
 
+
+class Calculation {
+    fun sequence(numbers: List<Double>, operators: List<String>): Double {
+        if (numbers.isEmpty()) return Const.INITIAL_DOUBLE_VALUE
+        return arithmetic(numbers, operators)
+    }
+
+    fun priority(numbers: List<Double>, operators: List<String>): Double {
+        val localOperators = operators.toMutableList()
+        val localNumbers = numbers.toMutableList()
+
+        firstOperation(localNumbers, localOperators)
+        return arithmetic(localNumbers, localOperators)
+    }
+
+    private fun arithmetic(numbers: List<Double>, operators: List<String>): Double {
         var result = numbers.first()
         var index = INITIAL_INDEX
 
         operators.forEach { operator ->
-            when(operator) {
-                ADDITION -> result += numbers[index++]
-                SUBTRACTION -> result -= numbers[index++]
-                MULTIPLICATION -> result *= numbers[index++]
-                DIVISION -> result /= numbers[index++]
+            when (operator) {
+                ADDITION -> result += numbers[++index]
+                SUBTRACTION -> result -= numbers[++index]
+                MULTIPLICATION -> result *= numbers[++index]
+                DIVISION -> result /= numbers[++index]
             }
         }
         return result
     }
 
-    fun priority(numbers: List<Int>, operators: List<String>) {}
+    private fun firstOperation(numbers: MutableList<Double>, operators: MutableList<String>) {
+        var index = INITIAL_INDEX
+        while (index++ < operators.size) {
+            val result = when (operators[index]) {
+                MULTIPLICATION -> numbers.removeAt(index) * numbers.removeAt(index)
+                DIVISION -> numbers.removeAt(index) / numbers.removeAt(index)
+                else -> continue
+            }
+            numbers.add(index, result)
+            operators.removeAt(index)
+        }
+    }
 
     companion object {
-        const val INITIAL_INDEX = 1
-        const val INITIAL_EMPTY_VALUE = 0
+        const val INITIAL_INDEX = 0
         const val ADDITION = "+"
         const val SUBTRACTION = "-"
         const val MULTIPLICATION = "*"
